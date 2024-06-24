1362446d08ff8f19747e34dff234c78d49c8844a
Youjie Zheng <Azure_stars@126.com>
2024-06-24 21:33:50 +0800

    tmp: test for SMP=4

--- scripts/test/app_test_for_monolithic.sh
+++ scripts/test/app_test_for_monolithic.sh
@@ -67,7 +67,7 @@ function test_one() {
     local args=$1
     local actual="$APP/actual.out"
     # 默认要用批量测试模式
-    args="$args ARCH=$ARCH APP_FEATURES=batch ACCEL=n"
+    args="$args ARCH=$ARCH SMP=4 APP_FEATURES=batch ACCEL=n"
     rm -f "$actual"
 
     MSG=
